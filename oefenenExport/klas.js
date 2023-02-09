19bede201b46da8b890088f6a9fa58dfcc6838df
Kacper-BTD <kacpers2703@gmail.com>
2023-02-09 16:22:28 +0100

    TestPush
    1e commit naar main

---
+++ oefenenExport/klas.js
@@ -0,0 +1,4 @@
+var student= require ('./student')
+
+console.log(student.age[2])
+console.log(student.name[2])
---
+++ oefenenExport/student.js
@@ -0,0 +1,7 @@
+var student = {
+    name:["Kacper","Fayaaz","Janno","Stephan"],
+    age: ["21","22","34","20"]
+}
+
+module.exports = student
+
